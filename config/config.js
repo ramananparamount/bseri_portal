bd02257c36a9e75a657f2d87086ce0fcc2ec0807
Ramanan <ramanan.skr@gmail.com>
2016-08-01 15:01:29 +0530

    Added About us Page

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     'secret': 'secretkey',
-    'dbconnectstr':'mongodb://localhost/bseri'
+    // 'dbconnectstr':'mongodb://localhost/bseri'
+    'dbconnectstr':'mongodb://para:para@ds139425.mlab.com:39425/bseri'
 
 };
