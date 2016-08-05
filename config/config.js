8faf62f8bfe8f2eedc0ee08c6824496f6b6d1417
Ramanan <ramanan.skr@gmail.com>
2016-08-05 11:46:37 +0530

    Configuration changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
 module.exports = {
 
     'secret': 'secretkey',
-    'dbconnectstr':'mongodb://localhost/bseri'
+    // 'dbconnectstr':'mongodb://localhost/bseri'
+    // 'dbconnectstr':'mongodb://para:para@ds139425.mlab.com:39425/bseri'
+    'dbconnectstr':'mongodb://para:para@ds139655.mlab.com:39655/bseri_tst'
 
 };
